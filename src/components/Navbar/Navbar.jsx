6f73df1efe9e75eaf2f66c8e0fc5daf8f9edfd3c
Itswali <luckywali0986@gmail.com>
2023-07-17 15:22:10 +0500

    add navbar and its sample styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,58 +4,50 @@ import logo from '../../assests/logo.jpg';
 import { Link } from 'react-router-dom';
 
 const Menu = () => (
-  <>
-    <p>
+  <ul className="navigation">
+    <li>
       <Link to="/">Home</Link>
-    </p>
-    <p>
+    </li>
+    <li>
       <Link to="/about">About</Link>
-    </p>
-    <p>
+    </li>
+    <li>
       <Link to="/product">Product</Link>
-    </p>
-  </>
+    </li>
+  </ul>
 );
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="navbar">
+    <div className="navbar main-menu">
       <div className="navbar-links">
         <div className="navbar-links-logo">
           <img src={logo} alt="logo" className="logo-navbar" />
         </div>
-        <div className="navbar-links_container">
+        <div className="navbar-links-container">
           <Menu />
         </div>
-      </div>
-      <div className="navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
       </div>
       <div className="navbar-menu">
         {toggleMenu ? (
           <RiCloseLine
-            color="fff"
+            color="#fff"
             size={27}
             onClick={() => setToggleMenu(false)}
           />
         ) : (
           <RiMenu3Line
-            color="fff"
+            color="#fff"
             size={27}
             onClick={() => setToggleMenu(true)}
           />
         )}
         {toggleMenu && (
-          <div className="navbar-menu_container scale-up-center">
-            <div className="navbar-menu_container-links">
+          <div className="navbar-menu-container scale-up-center">
+            <div className="navbar-menu-container-links">
               <Menu />
-              <div className="navbar-menu_container-links-sign">
-                <p>Sign in</p>
-                <button type="button">Sign up</button>
-              </div>
             </div>
           </div>
         )}
