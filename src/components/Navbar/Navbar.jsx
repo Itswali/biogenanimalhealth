e0018c746ad5171808d7d31c56cd2cadd6e5ee21
Itswali <luckywali0986@gmail.com>
2023-07-20 21:05:59 +0500

    about page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assests/Logo.avif';
+import logo from '../../assests/logo.jpeg';
 import { Link } from 'react-router-dom';
 
 const Menu = () => (
