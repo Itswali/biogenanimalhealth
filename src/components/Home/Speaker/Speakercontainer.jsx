55d183d4378f6f73d5366c00b07bf026f4775686
Itswali <luckywali0986@gmail.com>
2023-07-21 15:09:46 +0500

    updated speaker

--- src/components/Home/Speaker/Speakercontainer.jsx
+++ src/components/Home/Speaker/Speakercontainer.jsx
@@ -3,16 +3,16 @@ import Speaker from './Speaker';
 
 const speakers = [
   {
-    name: 'Khawaja Nouman Ajmal',
-    position: 'C.E.O Apex Biotical Solution',
-    quote: 'As a Dedicated client based enterprise, Apex Biotical Solution emphasis on the creation of pioneering worth to accomplish sustainable profit growth of the Livestock and poultry farmers. Needs brilliant individuals who can see into the future and bring about changes in dairy and poultry sector. Apex Biotical solution thanks each of our customer, employee, poultry and dairy farmers for your commitment, partnership and persistent support throughout the year.',
-    image: 'http://absvet.org/images/resource/team-1.png',
+    name: 'Name of Speaker 1',
+    position: 'Position of speaker 1',
+    quote: 'A quote or message by the author about the company or its mission that will be displayed in this area',
+    image: 'https://www.shutterstock.com/image-vector/business-man-icon-trendy-flat-260nw-1041223936.jpg',
   },
   {
-    name: 'Mr. Jalil ur Rehman',
-    position: 'Managing Director Apex Biotical Solution',
-    quote: 'At Apex, we envision to make sustainable, safe and profitable livestock and poultry business a reality. “Try try again” is the key to success. You never lose until you accept defeat. If you want to be strong, successful and want to move forward as compared to others then keep trying to change for betterment and adopt new efforts. Those organization are successful who are sustainable and inplace innovative ideas related to poultry and livestock medicines and modern farming. ',
-    image: 'http://absvet.org/images/resource/team-2.png',
+    name: 'Name of Sepeaker 2',
+    position: 'Posotionn of speaker 2',
+    quote: 'A quote or message by the author about the company or its mission that will be displayed in this area',
+    image: 'https://www.shutterstock.com/image-vector/business-man-icon-trendy-flat-260nw-1041223936.jpg',
   },
   // Add more speakers as needed
 ];
